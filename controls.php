1d34e67eb91765594845e7db9e400609707b6f99
Ken Irwin <kirwin@wittenberg.edu>
2019-06-18 07:39:25 -0700

    controls.php shows image previews

--- controls.php
+++ controls.php
@@ -1,3 +1,36 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script>
+   $(document).ready(function() {
+       $('#old').change(function() {
+	   var img = $(this).val();
+	   LoadPhoto('photo1',img,'upload');
+	 });
+       
+       $('#new').change(function() {
+	   console.log('change is coming');
+	   var img = $(this).val();
+	   LoadPhoto('photo2',img,'upload');
+	 });
+       $('#rotation').change(function() {
+	   var rot = $(this).val() * -1;//css reckons degrees opposite of php
+	   $('#photo2 img').css('transform', 'rotate('+rot+'deg)');
+	 });
+       function LoadPhoto(destination,filename,path) {
+	 $('#'+destination).empty().append('<img src="deliver.php?filename='+filename+'">');	 
+       }
+     });
+</script>
+
+<style>
+.preview img {
+ width:250px;
+  }
+#pair div {
+display: inline;
+}
+</style>
+<h1>Homecoming Photo Pairing</h1>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -15,9 +48,9 @@ foreach ($rows as $row) {
 }
 
 print '<form action="pair.php">'.PHP_EOL;
-print '<select name="old">'.$opts.'</select>';
-print '<select name="new">'.$opts.'</select>';
-print '<select name="rotation">
+print '<select name="old" id="old"><option>Select one</option>'.$opts.'</select>';
+print '<select name="new" id="new"><option>Select one</option>'.$opts.'</select>';
+print '<select name="rotation" id="rotation">
 <option value="0">None</option>
 <option value="-90">Clockwise</option>
 <option value="90">Counterclockwise</option>
@@ -26,3 +59,7 @@ print '<select name="rotation">
 print '<input type="submit">'.PHP_EOL;
 print '</form>'.PHP_EOL;
 ?>
+<div id="pair">
+<div id="photo1" class="preview"></div>
+<div id="photo2" class="preview"></div>
+</div>
