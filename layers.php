c865485059afb31eda373bee392d571aeb9f8f5f
Ken Irwin <kirwin@wittenberg.edu>
2019-06-28 13:53:00 -0700

    layers.php returns photo pairs by email

--- layers.php
+++ layers.php
@@ -7,6 +7,9 @@ require_once 'vendor/autoload.php';
 
 use wittproj\LayeredImage;
 use wittproj\Database;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Email\Parse; 
 
 /* set variables */
 if ($_REQUEST['path'] == 'extracts') {
@@ -37,16 +40,44 @@ $font = FONT;
 imagettftext($img->image, 25, 0, 95, 100, $white, $font, $text);
 
 /* output file */
-$filename = preg_replace('/\.(jpg|png)/','',$old_nopath) 
+$filename = preg_replace('/\.(jpg|jpeg|png)/i','',$old_nopath) 
   . '__' 
-  . preg_replace('/\.(jpg|png)/','',$new_nopath)
+  . preg_replace('/\.(jpg|jpeg|png)/i','',$new_nopath)
   . '.png';
 $filename = ValidateFilename($filename);
 $db = new Database;
 $db->submitPair($filename, $old_nopath, $new_nopath, $old_table);
 imagepng($img->image, OUTPUT_FILE_PATH . $filename);
 print '<img src="'.OUTPUT_HTTP_PATH.$filename.'" />'.PHP_EOL;
+$emails = Parse::getInstance()->parse($db->getSubmissionEmail($new_nopath));
+$mailTo = ($emails['email_addresses'][0]['simple_address']);
 
+$filepath = OUTPUT_FILE_PATH.$filename;
+/*
+print "<li>$filepath</li>";
+print "<li>$filename</li>";
+print "<li>$mailTo</li>";
+print "<li>".MAIL_FROM."</li>";
+print "<li>".MAIL_SUBJECT."</li>";
+print "<li>".MAIL_BODY."</li>";
+*/
+if (DoMail($mailTo,OUTPUT_FILE_PATH.$filename,$filename)) {
+  print "<h1>Mail Sent</h1>";
+}
+
+else {
+  print "<h1>Could NOT send mail!</h1>";
+}
+
+function DoMail ($mailTo,$filepath,$filename) {
+  $email = new PHPMailer();
+  $email->SetFrom(MAIL_FROM); //Name is optional
+  $email->Subject   = MAIL_SUBJECT;
+  $email->Body      = MAIL_BODY;
+  $email->AddAddress( $mailTo );
+  $email->AddAttachment( $filepath , $filename );
+  return $email->Send();
+}
 
 function ValidateFilename($filename) {
   if (preg_match('/[^a-zA-Z0-9\.\-\_]/',$filename)) {
