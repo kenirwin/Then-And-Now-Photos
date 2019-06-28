f0f6b588b7657b6b1bd17234fc622d6794b100f0
Ken Irwin <kirwin@wittenberg.edu>
2019-06-28 12:02:34 -0700

    removed blank line in DatabaseTest

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -3,7 +3,6 @@ namespace wittproj\test;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 require (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'); 
 
 use PHPUnit\Framework\TestCase;
