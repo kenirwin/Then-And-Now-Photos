3b411ec1d34482efe08d94b4a6da0c5c27e35d15
Ken Irwin <kirwin@wittenberg.edu>
2019-08-30 13:26:50 -0700

    added navigation

---
+++ nav.php
@@ -0,0 +1,20 @@
+<style>
+ul#nav li { 
+display: inline;
+margin-right: 1.5em;
+font-weight: bold;
+}
+ul#nav li a, ul#nav li a:visited {
+color: #900;
+}
+ul#nav li a:hover {
+background-color: #900;
+color: white;
+}
+</style>
+
+<ul id="nav">
+   <li><a href="index.php">Pair with Yearbook Photos</a>
+   <li><a href="zoom.php">Extract from Group Photos</a>
+   <li><a href="index.php?path=extracts">Pair Extracts of Groups</a>
+</ul>
