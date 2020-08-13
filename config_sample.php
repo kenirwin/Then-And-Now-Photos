c2e1596b8f178c46cbd138acfe198200f84f8b2d
Ken Irwin <kirwin@wittenberg.edu>
2020-08-13 12:56:06 -0700

    updated config)sample for less data entry

--- config_sample.php
+++ config_sample.php
@@ -7,10 +7,12 @@ define('CHARSET','utf8');
 define('DSN', 'mysql:host='.HOST.';dbname='.DATABASE.';charset='.CHARSET);
 
 define('SECURE_UPLOAD_PATH',''); //outside the public web space
-define('ARCHIVES_FILE_PATH',''); //in the web space
-define('EXTRACT_FILE_PATH',''); //in the web space
-define('OUTPUT_FILE_PATH',''); //in the web space 
-define('OUTPUT_HTTP_PATH',''); //corresponds to OUTPUT_FILE_PATH
+define('WEB_FILE_PATH',''); // absolute file path on the server-side, no trailing slash, e.g. '/home/myaccount/public_html/projects/now-and-then'
+define('WEB_HTTP_PATH',''); // URL path, no trailing slash, corresponds to the previous entry, e.g. '/projects/now-and-then'
+define('ARCHIVES_FILE_PATH', WEB_FILE_PATH.'/images/archives/'); //in the web space
+define('EXTRACT_FILE_PATH', WEB_FILE_PATH.'/images/extracts/'); //in the web space
+define('OUTPUT_FILE_PATH', WEB_FILE_PATH.'/images/composites/'); //in the web space 
+define('OUTPUT_HTTP_PATH', WEB_HTTP_PATH.'/images/composites/'); //corresponds to OUTPUT_FILE_PATH
 define('ALLOWED_FILE_TYPES',array('image/png','image/jpeg'));
 
 define('MAIL_FROM',''); //email address
